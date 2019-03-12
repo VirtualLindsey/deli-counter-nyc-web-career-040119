b077033493cd8729149832f90939c1ac7b7b0382
L D <tapircode@gmail.com>
2019-03-12 03:01:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,21 @@
 katz_deli = []
 
-def line(line)
-  if line.l
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    line = ""
+    katz_deli.each_with_index do |name, index|
+      line += "#{index+1}. #{name} "
+    end
+    puts line
+  end
+end
+
+def now_serving()
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
